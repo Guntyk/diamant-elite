8944549876e5b27d61baaea8478396df4ab6e8e3
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-10-13 19:52:25 +0300

    create small device warning

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,18 @@
 import { Sidebar } from 'components/Sidebar';
+import styles from 'App.scss';
 
 export default function App() {
-  return <Sidebar />;
+  return (
+    <>
+      <div className={styles.app}>
+        <Sidebar />
+      </div>
+
+      <div className={styles.warning}>
+        <span>😕</span>
+        <p>Вибачте, цей пристрій замалий</p>
+        <p className={styles.text}>Чутки кажуть, що з комп'ютера тут краще видно 🤔</p>
+      </div>
+    </>
+  );
 }
